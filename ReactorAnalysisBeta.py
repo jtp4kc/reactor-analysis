9b984e0ac092bb6ade211a7119ab0f6db964dd73
Tyler P <jtp4kc@virginia.edu>
2019-03-14 00:54:21 -0400

    integrated and tested reading from file to get reaction details

--- ReactorAnalysisBeta.py
+++ ReactorAnalysisBeta.py
@@ -11,7 +11,7 @@ PATH = os.path.join("C:/", "Users", "adjun_000", "Documents", "Grad School",
 def_file = "analysis_settings.txt"
 definitions = None
 config_files = [
-    ("", "20190205_5MgOonSiO2-IWI", "setup.txt"),
+    ("", "20190205_5MgOonSiO2-IWI", "details.txt"),
     ]
 
 # Constants
@@ -20,6 +20,11 @@ T = 273.15 + 25  # K, ambient
 P = 101.325  # kPa, ambient
 Ts = 273.15  # K, standard
 Ps = 101.325  # kPa, standard
+
+# Test Flags
+TEST_CONFIG = False
+TEST_PEAKS = False
+TEST_OUTPUT = False
 
 class Peaks():
     def init(self, rxn_data):
@@ -269,6 +274,10 @@ class Reference(_SetupFile):
             if cn.name == "focus":
                 self.focus = cn
 
+    def save(self, filename):
+        if filename is not None:
+            self._write(filename)
+
 class Details(_SetupFile):
 
     def __init__(self):
@@ -395,6 +404,12 @@ class Details(_SetupFile):
                 self.comps = cn
             if cn.name == "flow":
                 self.flows.append(cn)
+
+    def save(self, filename):
+        if filename is None:
+            self._test()
+        else:
+            self._write(filename)
 
 class Temp():
 
@@ -529,7 +544,6 @@ class Pres():
         return str(self.pres) + " Pa"
 
 def test_config():
-    _MODE_WRITE = True
     test = Details()
     test._test()
     test._write(".test_reactor_conditions_output.txt")
@@ -542,70 +556,13 @@ def test_config():
     temp._load("rc_template.txt")
     temp._write("rc_template-test.txt")
 
-class Config():
-    def __init__(self, filename):
-        self.filename = filename
-        self.prefix = "_"
-        self.root = Config.Subentry()
-
-    def save(self):
-        base = os.path.basename(self.filename)
-        filename = os.path.join(os.path.dirname(self.filename),
-                                self.prefix + base)
-        with open(filename) as outfile:
-            outfile.write("#Reactor Analysis Beta - Configuration File\n\n")
-            self._recursive_write("", outfile, self.root)
-            outfile.close()
-
-    def read(self):
-        pass
-
-    def _recursive_write(self, pre, out, entry):
-        for comment in entry.cmnt:
-            out.write(pre + "#" + comment + "\n")
-        for flag in entry.flag:
-            out.write(pre + flag + "\n")
-        for key in entry.attr:
-            if not key.startswith("_"):
-                val = entry.attr[key]
-                out.write(pre + key + " = " + val)
-                if "_" + key in entry.attr:
-                    cmnt = entry.attr["_" + key]
-                    out.write(" #" + cmnt + "\n")
-                else:
-                    out.write("\n")
-        for item in entry.item:
-            out.write(pre + item.name + "\n")
-            self._recursive_write(pre + "\t", out, item)
-
-    class Subentry():
-        def __init__(self):
-            self.attr = dict()
-            self.item = list()
-            self.flag = list()
-            self.cmnt = list()
-            self.name = "."
-
-class Common():
-    def __init__(self, file):
-        self.config = Config(file)
-        if os.path.exists(file):
-            self.config.read()
-
-        self.data = self.config.root()
-        found = False
-        for item in self.data.item:
-            if "focus" == item.name:
-                found = True
-                break
-        if not found:
-            item = Config.Subentry()
-            item.name = "focus"
-            self.data.item.append(item)
-
 class Setup():
     def __init__(self, config):
-        pass
+        self.info = Details()
+        self.info.load(config)
+        if TEST_CONFIG:
+            self.info.attr["orig_file"] = config
+            self.info.save("TEST_" + config)
 
     def load_data(self):
         return None
@@ -644,7 +601,15 @@ def analyze(config):
     output_manager.save_analysis(setup, rxn_data, peak_handler)
 
 if __name__ == '__main__':
-    definitions = Common(os.path.join(PATH, def_file))
+    ref_path = os.path.join(PATH, def_file)
+    definitions = Reference()
+    if os.path.exists(ref_path):
+        definitions.load(ref_path)
+    if TEST_CONFIG:
+        definitions.attr["orig_path"] = repr(ref_path)
+        definitions.save(os.path.join(PATH, "TEST_" + def_file))
+        os.chdir(PATH)
+        test_config()
     for cf in config_files:
         folder = os.path.join(PATH, cf[0], cf[1])
         os.chdir(folder)
